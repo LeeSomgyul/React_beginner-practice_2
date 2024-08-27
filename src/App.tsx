8487bc8edc6839f2a88c8320327c2eada199fdd2
LeeSomgyul <rmawl8600@naver.com>
2024-08-28 02:13:31 +0900

    0828 연습
    'react-beautiful-dnd' 사용해보기(셋팅, css, placeholders)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,70 @@
-import { useRecoilState } from "recoil";
-import {minuteState, hourSelector} from "./atom";
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
+import styled from "styled-components";
 
-function App (){
-	const [minutes, setMinutes] = useRecoilState(minuteState);//분->시로 변경하는 state
-	const [hours, setHours] = useRecoilState(hourSelector);//시->분으로 변경하는 state
-	const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-		setMinutes(+event.currentTarget.value);
-	};
-	const onHourChange = (event: React.FormEvent<HTMLInputElement>) => {
-		setHours(+event.currentTarget.value);
-	};
+const Wrapper = styled.div`
+	display: flex;
+	max-width: 480px;
+	width: 100%;
+	margin: 0 auto;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+`;
+
+const Boards = styled.div`
+	display: grid;
+	width: 100%;
+	grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+	padding-top: 30px;
+	padding: 20px 10px;
+	background-color: ${(props) => props.theme.boardColor};
+	border-radius: 5px;
+	min-height: 200px;
+`;
+
+const Card = styled.div`
+	border-radius: 5px;
+	padding: 10px 10px;
+	margin-bottom: 5px;
+	background-color: ${(props) => props.theme.cardColor};
+`;
+
+const toDos = ['a', 'b', 'c', 'd', 'e'];
+
+function App(){
+	const onDragEnd = () => {
+
+	}
 
 	return(
-		<div>
-			<input value={minutes} onChange={onMinuteChange} type="text" placeholder="minute"/>
-			<input value={hours} onChange={onHourChange} type="text" placeholder="hour"/>
-		</div>
+		//필수: onDrageEnd(드래그를 놓을 때 발생하는 함수), children(<DragDropContext> 내부 코드. ex)<div>)
+		<DragDropContext onDragEnd={onDragEnd}>
+			<Wrapper>
+				<Boards>
+					{/*필수: droppableId(각 Droppable에 이름 붙이기), children(<Droppable> 내부 코드, 단 함수 형식어어야 한다.) */}
+					<Droppable droppableId="one">
+						{(provided) => (
+						<Board {...provided.droppableProps} ref={provided.innerRef}>
+							{/*필수: draggableId, index(정렬을 위한 순서), children*/}
+							{toDos.map((toDo, index) => (
+							<Draggable draggableId={toDo} index={index}>
+								{(provided) => (
+									<Card ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+										{toDo}
+									</Card>
+								)}
+							</Draggable>
+							))}
+							{provided.placeholder}
+						</Board>
+						)}
+					</Droppable>
+				</Boards>
+			</Wrapper>
+		</DragDropContext>
 	);
 }
 
