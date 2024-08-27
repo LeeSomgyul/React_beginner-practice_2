7417edfe2c5a1feb68d2785c26fb74298032c967
LeeSomgyul <rmawl8600@naver.com>
2024-08-27 23:37:57 +0900

    0827 연습
    Recoil의 selector set기능 연습

--- src/atom.tsx
+++ src/atom.tsx
@@ -1,34 +1,18 @@
 import {atom, selector} from "recoil";
 
-//enum: 어디서든 값을 쓸 수 있게끔 해준다. 값을 숫자로 저장("TO_DO" = 0)하기 때문에 stirng으로 저장을 원한다면 아래와 같이 작성해야한다.
-export enum Categoryies {
-    "TO_DO" = "TO_DO",
-    "DOING" = "DOING",
-    "DONE" = "DONE",
-}
-
-export interface IToDo {
-    id: number;
-    text: string;
-    category: Categoryies;//category는 3가지 string만 가능
-}
-
-export const categoryState = atom<Categoryies>({
-    key: "category",
-    default: Categoryies.TO_DO,//기본값은 "TO_DO"
+export const minuteState = atom({
+    key: "minuteState",
+    default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-    key: "toDo",
-    default: [],
-});
-
-//category별 todo 분류작업
-export const toDoSelector = selector({
-    key: "toDoSelector",
-    get: ({get}) => {
-        const toDos = get(toDoState);
-        const category = get(categoryState);
-        return toDos.filter((toDo) => toDo.category === category);
+export const hourSelector = selector<number>({
+    key: "hourSelector",
+    get: ({get}) => {//atom값을 가져오는 get
+        const minute = get(minuteState);
+        return minute / 60;
+    },
+    set: ({set}, newValue) => {//atom값을 수정하는 set
+        const minute = Number(newValue) * 60;
+        set(minuteState, minute);//minute를 저장하는 minuteState의 default값을 minute값으로 변경
     },
 });
